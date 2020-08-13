c8239cd56ca31cad8d6cffe8e994bb875968d5d2
rashadmin <69635878+rashadmin@users.noreply.github.com>
2020-08-13 14:59:00 +0100

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,16 @@
+def Fibonacci(num):
+   if num <= 1:
+       return n
+   else:
+       return(fibonacci(num-1) + fibonacci(num-2))
+
+number_of terms_ = int(input("Enter the number of terms: "
+
+# checks if the number of terms is valid
+if number_of_terms <= 0:
+    print("Please input a positive integer")
+else:
+    print("Fibonacci sequence: ")
+number = range(number_of_terms)
+    while i < number:
+        print(Fibonacci(i))
